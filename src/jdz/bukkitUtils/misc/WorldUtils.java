3452fdbd9a9cc825fbb7b30f1c6e1acb01ae3d94
Jonodonozym <jaidencolebaker@gmail.com>
2017-12-02 12:11:03 +1300

    Fixed getnearbyplayers IllegalArgumentException for multi-worlds

--- src/jdz/bukkitUtils/misc/WorldUtils.java
+++ src/jdz/bukkitUtils/misc/WorldUtils.java
@@ -82,7 +82,7 @@ public final class WorldUtils {
 	public static Set<Player> getNearbyPlayers(Location location, int range){
 		Set<Player> nearbyPlayers = new HashSet<Player>();
 		for (Player player: Bukkit.getServer().getOnlinePlayers())
-			if (player.getLocation().distance(location) < range && player.getWorld().equals(location.getWorld()))
+			if (player.getWorld().equals(location.getWorld()) && player.getLocation().distance(location) < range)
 				nearbyPlayers.add(player);
 		return nearbyPlayers;
 	}
