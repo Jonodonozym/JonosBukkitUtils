710cf99f6cd38b525cab08be06d6ffe5b5756733
Jonodonozym <jaidencolebaker@gmail.com>
2017-12-03 14:18:33 +1300

    Added listener to prevent clients from crashing w anvil inventories

--- src/jdz/bukkitUtils/misc/Enchantment.java
+++ src/jdz/bukkitUtils/misc/Enchantment.java
@@ -2,14 +2,35 @@
 package jdz.bukkitUtils.misc;
 
 import java.lang.reflect.Field;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import jdz.bukkitUtils.JonosBukkitUtils;
+import jdz.bukkitUtils.fileIO.FileLogger;
 
 public abstract class Enchantment extends org.bukkit.enchantments.Enchantment{
 	
-	public Enchantment(int id) {
+	private static final Set<Enchantment> enchantments = new HashSet<Enchantment>();
+	
+	public Enchantment(JavaPlugin plugin, int id) {
 		super(id);
+		
+		if (enchantments.contains(this))
+			throw new IllegalArgumentException("An enchantment with an id of "+id+" has already been registered!");
+		
+		enchantments.add(this);
+		register(plugin);
 	}
 	
-	public void register() {
+	private void register(JavaPlugin plugin) {
 		try {
 			try {
 				Field f = Enchantment.class.getDeclaredField("acceptingNew");
@@ -21,10 +42,50 @@ public abstract class Enchantment extends org.bukkit.enchantments.Enchantment{
 			try {
 				Enchantment.registerEnchantment(this);
 			} catch (IllegalArgumentException e) {
-				// if this is thrown it means the id is already taken.
+				new FileLogger(plugin).createErrorLog(e);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean equals(Object other) {
+		if (!(other instanceof Enchantment))
+			return false;
+		return ((Enchantment)other).getId() == getId();
+	}
+	
+	@Override
+	@SuppressWarnings("deprecation")
+	public int hashCode() {
+		return getId();
+	}
+	
+	
+	
+	
+	
+	
+	static {
+		Bukkit.getPluginManager().registerEvents(new AnvilCrashPreventer(), JonosBukkitUtils.instance);
+	}
+	
+	private static class AnvilCrashPreventer implements Listener{
+		
+		@EventHandler
+		public void onInventoryClick(InventoryClickEvent event) {
+			if (!(event.getInventory() instanceof AnvilInventory))
+				return;
+			
+			for (org.bukkit.enchantments.Enchantment enchant: event.getCurrentItem().getEnchantments().keySet())
+				if (enchantments.contains(enchant)) {
+					event.getWhoClicked().sendMessage(ChatColor.RED+"Items with custom enchantments cannot be used on anvils!");
+					event.setCancelled(true);
+					return;
+				}
 		}
 	}
 }
