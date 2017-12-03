582e75dd4a30a48579d7c653471ef9e9473bce4d
Jonodonozym <jaidencolebaker@gmail.com>
2017-12-03 15:42:46 +1300

    Added decoding of roman numbers

--- src/jdz/bukkitUtils/misc/RomanNumber.java
+++ src/jdz/bukkitUtils/misc/RomanNumber.java
@@ -17,22 +17,38 @@ import java.util.TreeMap;
  * @author Jonodonozym
  */
 public final class RomanNumber {
-    private final static TreeMap<Integer, String> map = new TreeMap<Integer, String>();
 
-    static {
-        map.put(1000, "M");
-        map.put(900, "CM");
-        map.put(500, "D");
-        map.put(400, "CD");
-        map.put(100, "C");
-        map.put(90, "XC");
-        map.put(50, "L");
-        map.put(40, "XL");
-        map.put(10, "X");
-        map.put(9, "IX");
-        map.put(5, "V");
-        map.put(4, "IV");
-        map.put(1, "I");
+
+	private static final TreeMap<String, Integer> StringToInt = new TreeMap<String, Integer>();
+	private static final TreeMap<Integer, String> IntToString = new TreeMap<Integer, String>();
+    
+   static {
+	    StringToInt.put("IV", 4);
+	    StringToInt.put("IX", 9);
+	    StringToInt.put("XL", 40);
+	    StringToInt.put("CD", 400);
+	    StringToInt.put("CM", 900);
+	    StringToInt.put("C", 100);
+	    StringToInt.put("M", 1000);
+	    StringToInt.put("I", 1);
+	    StringToInt.put("V", 5); 
+	    StringToInt.put("X", 10);
+	    StringToInt.put("L", 50);
+	    StringToInt.put("D", 500);
+
+	    IntToString.put(1000, "M");
+	    IntToString.put(900, "CM");
+        IntToString.put(500, "D");
+        IntToString.put(400, "CD");
+        IntToString.put(100, "C");
+        IntToString.put(90, "XC");
+        IntToString.put(50, "L");
+        IntToString.put(40, "XL");
+        IntToString.put(10, "X");
+        IntToString.put(9, "IX");
+        IntToString.put(5, "V");
+        IntToString.put(4, "IV");
+        IntToString.put(1, "I");
     }
 
     /**
@@ -44,10 +60,24 @@ public final class RomanNumber {
     	if (number < 1)
     		return "";
     	
-        int l =  map.floorKey(number);
+        int l =  IntToString.floorKey(number);
         if ( number == l ) {
-            return map.get(number);
+            return IntToString.get(number);
         }
-        return map.get(l) + of(number-l);
+        return IntToString.get(l) + of(number-l);
     }
+    
+    public static int decode(String roman) {
+        int result = 0;
+        for (String s : StringToInt.keySet()) {
+         result += countOccurrences(roman, s) * StringToInt.get(s);
+         roman = roman.replaceAll(s, "");
+        }
+
+        return result;
+    }
+    
+    private  static int countOccurrences(String main, String sub) {
+    	   return (main.length() - main.replace(sub, "").length()) / sub.length();
+    } 
 }
