fb95934a3dd50e2dc890da8cf0254d9781907b61
Jonodonozym <jaidencolebaker@gmail.com>
2017-12-03 15:16:17 +1300

    Added Color Utils

---
+++ src/jdz/bukkitUtils/misc/ColorUtil.java
@@ -0,0 +1,27 @@
+package jdz.bukkitUtils.misc;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ColorUtil {
+	
+    private static String translateAlternateColorCodes(char altColorChar, String textToTranslate) {
+        char[] b = textToTranslate.toCharArray();
+        for (int i = 0; i < b.length - 1; i++) {
+            if (b[i] == altColorChar && "0123456789AaBbCcDdEeFfKkLlMmNnOoRr".indexOf(b[i+1]) > -1) {
+                b[i] = '§';
+                b[i+1] = Character.toLowerCase(b[i+1]);
+            }
+        }
+        return new String(b);
+    }
+
+    public static String translate(String string) {
+        return translateAlternateColorCodes('&', string);
+    }
+
+    public static List<String> translate(List<String> string) {
+        return string.stream().map(ColorUtil::translate).collect(Collectors.toCollection(ArrayList::new));
+    }
+}
