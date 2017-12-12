7168c15099f0870557c1de00bd527086a1f5b326
Jonodonozym <jaidencolebaker@gmail.com>
2017-12-12 15:15:06 +1300

    Added TimeUtils

---
+++ src/jdz/bukkitUtils/misc/utils/TimeUtils.java
@@ -0,0 +1,33 @@
+
+package jdz.bukkitUtils.misc.utils;
+
+public class TimeUtils {
+	
+	public static String timeFromMinutes(int minutes) {
+		return timeFromSeconds(minutes*60);
+	}
+	
+	public static String timeFromSeconds(int totalSeconds){
+		int days = totalSeconds / 86400;
+		int hours = (totalSeconds % 86400 ) / 3600 ;
+		int minutes = ((totalSeconds % 86400 ) % 3600 ) / 60 ;
+		int seconds = ((totalSeconds % 86400 ) % 3600 ) % 60 ;
+		
+		String rs = "";
+		if (days > 0)
+			rs = rs+days+"d ";
+		if (hours > 0)
+			rs = rs+hours+"h ";
+		if (minutes > 0)
+			rs = rs+minutes+"m ";
+		if (seconds > 0)
+			rs = rs+seconds+"s ";
+		
+		if (rs.equals(""))
+			rs = "0s";
+		else
+			rs = rs.substring(0, rs.length()-1);
+		
+		return rs;
+	}
+}
