2d96c0af538326b104043bc3f119fe140faefc5a
Jonodonozym <jaidencolebaker@gmail.com>
2018-02-23 20:44:14 +1300

    Added cancellable interface

--- src/jdz/bukkitUtils/events/custom/PotionDrinkEvent.java
+++ src/jdz/bukkitUtils/events/custom/PotionDrinkEvent.java
@@ -2,7 +2,6 @@
 package jdz.bukkitUtils.events.custom;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -13,22 +12,30 @@ import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffect;
 
+import com.sun.xml.internal.ws.api.Cancelable;
+
 import jdz.bukkitUtils.events.Event;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @SuppressWarnings("deprecation")
 @AllArgsConstructor
-public class PotionDrinkEvent extends Event{
+public class PotionDrinkEvent extends Event implements Cancelable{
 	@Getter private final Collection<PotionEffect> effects;
 	@Getter private final Player player;
+	private final PlayerItemConsumeEvent parent;
+	
+	@Override
+	public void cancel(boolean cancel) {
+		parent.setCancelled(cancel);
+	}
 	
 	static class PotionDrinkEventListener implements Listener {
 		@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 		public void onDrink(PlayerItemConsumeEvent event) {
 			if (event.getItem().getType() == Material.POTION) {
 				Potion potion = Potion.fromItemStack(event.getItem());
-				new PotionDrinkEvent(potion.getEffects(), event.getPlayer());
+				new PotionDrinkEvent(potion.getEffects(), event.getPlayer(), event).call();
 			}
 		}
 	}
