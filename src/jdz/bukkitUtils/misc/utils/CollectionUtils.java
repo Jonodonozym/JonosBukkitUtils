968163ade6cd93293c2221b8411e3c7a991c786e
Jonodonozym <jaidencolebaker@gmail.com>
2018-03-04 16:48:21 +1300

    Added CollectionUtils

---
+++ src/jdz/bukkitUtils/misc/utils/CollectionUtils.java
@@ -0,0 +1,51 @@
+
+package jdz.bukkitUtils.misc.utils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class CollectionUtils {
+
+	@SuppressWarnings("unchecked")
+	public static <K, T extends Number> void addToAll(Map<K, T> map, T value) {
+		for (K key : map.keySet())
+			map.put(key, (T) addNumbers(map.get(key), value.doubleValue()));
+	}
+
+	public static <K, T extends Number> void removeNonPositive(Map<K, T> map) {
+		Set<K> toRemove = new HashSet<K>();
+		for (K key : map.keySet())
+			if (map.get(key).doubleValue() <= 0)
+				toRemove.add(key);
+		for (K key : toRemove)
+			map.remove(key);
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T[] concatArrays(T[] first, T[]... rest) {
+		int totalLength = first.length;
+		for (T[] array : rest)
+			totalLength += array.length;
+			
+		T[] result = Arrays.copyOf(first, totalLength);
+		int offset = first.length;
+		for (T[] array : rest) {
+			System.arraycopy(array, 0, result, offset, array.length);
+			offset += array.length;
+		}
+		return result;
+	}
+
+	private static Number addNumbers(Number a, Number b) {
+		if (a instanceof Double || b instanceof Double)
+			return new Double(a.doubleValue() + b.doubleValue());
+		else if (a instanceof Float || b instanceof Float)
+			return new Float(a.floatValue() + b.floatValue());
+		else if (a instanceof Long || b instanceof Long)
+			return new Long(a.longValue() + b.longValue());
+		else
+			return new Integer(a.intValue() + b.intValue());
+	}
+}
