99a17c968a8d52f00884d43f5c45ee7c09b642f2
bakerjaid <jaidencolebaker@gmail.com>
2018-05-28 20:58:46 +1200

    Added Random as static access to java.lang.random

---
+++ src/jdz/bukkitUtils/misc/Random.java
@@ -0,0 +1,39 @@
+
+package jdz.bukkitUtils.misc;
+
+public class Random {
+	private static final java.util.Random random = new java.util.Random();
+	private Random() {}
+
+	public static int nextInt(int bound) {
+		return random.nextInt(bound);
+	}
+	
+	public static long nextLong(long bound) {
+		return (long)nextDouble(bound);
+	}
+	
+	public static boolean nextBoolean() {
+		return random.nextBoolean();
+	}
+
+	public static float nextFloat() {
+		return random.nextFloat();
+	}
+	
+	public static float nextFloat(float bound) {
+		return random.nextFloat() * bound;
+	}
+
+	public static double nextDouble() {
+		return random.nextDouble();
+	}
+
+	public static double nextDouble(double bound) {
+		return random.nextDouble() * bound;
+	}
+	
+	public static void setSeed(Long seed) {
+		random.setSeed(seed);
+	}
+}
