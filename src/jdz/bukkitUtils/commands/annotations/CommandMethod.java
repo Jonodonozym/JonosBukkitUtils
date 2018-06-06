49c4c7262e84c574b9e4a698a28fab6b1530bd67
bakerjaid <jaidencolebaker@gmail.com>
2018-06-06 12:33:31 +1200

    parseflags defaulted to false

--- src/jdz/bukkitUtils/commands/annotations/CommandMethod.java
+++ src/jdz/bukkitUtils/commands/annotations/CommandMethod.java
@@ -11,5 +11,5 @@ import java.lang.annotation.Target;
 @Target(METHOD)
 public @interface CommandMethod {
 	public boolean withSender() default true;
-	public boolean parseFlags() default true;
+	public boolean parseFlags() default false;
 }
