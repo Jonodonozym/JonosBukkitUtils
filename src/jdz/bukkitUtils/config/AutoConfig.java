7dba992045ff6967cc08133ab010d4b92ddfe3e0
bakerjaid <jaidencolebaker@gmail.com>
2019-02-27 18:14:04 +1300

    Added AutoConfig support for private static fields

--- src/jdz/bukkitUtils/config/AutoConfig.java
+++ src/jdz/bukkitUtils/config/AutoConfig.java
@@ -3,6 +3,7 @@ package jdz.bukkitUtils.config;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +21,20 @@ import jdz.bukkitUtils.misc.Config;
 import lombok.Getter;
 
 public abstract class AutoConfig implements Listener {
+	private static final Field modifiersField;
+	static {
+		Field f = null;
+		try {
+			f = Field.class.getDeclaredField("modifiers");
+			f.setAccessible(true);
+		}
+		catch (ReflectiveOperationException e) {
+			e.printStackTrace();
+		}
+		modifiersField = f;
+	}
+
+
 	@Getter private final Plugin plugin;
 	@Getter private final String fileName;
 	@Getter private final String section;
@@ -42,10 +57,22 @@ public abstract class AutoConfig implements Listener {
 		this.fileName = file;
 
 		for (Field field : getClass().getDeclaredFields())
-			if (field.getAnnotation(NotConfig.class) == null) {
-				field.setAccessible(true);
-				fields.add(field);
-			}
+			if (field.getAnnotation(NotConfig.class) == null)
+				if (setModifiable(field))
+					fields.add(field);
+
+	}
+
+	private boolean setModifiable(Field field) {
+		try {
+			field.setAccessible(true);
+			modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+			return true;
+		}
+		catch (ReflectiveOperationException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	public void register() {
@@ -96,29 +123,30 @@ public abstract class AutoConfig implements Listener {
 	}
 
 	public void reloadConfig(ConfigurationSection section) {
-		try {
-			for (Field field : fields) {
+		for (Field field : fields)
+			try {
 				if (section.contains(field.getName())) {
+					field.setAccessible(true);
 					Object val = AutoConfigIO.parse(field.getGenericType(), field.getType(), section, field.getName());
 					field.set(this, val);
 				}
 			}
-		}
-		catch (ReflectiveOperationException e) {
-			e.printStackTrace();
-		}
+			catch (ReflectiveOperationException e) {
+				e.printStackTrace();
+			}
+			catch (NullPointerException e) {}
 	}
 
 	protected void writeConfig(ConfigurationSection section) {
-		try {
-			for (Field field : fields) {
+		for (Field field : fields)
+			try {
+				field.setAccessible(true);
 				Object val = field.get(this);
 				AutoConfigIO.save(field.getGenericType(), field.getType(), section, field.getName(), val);
 			}
-		}
-		catch (ReflectiveOperationException e) {
-			e.printStackTrace();
-		}
+			catch (ReflectiveOperationException e) {
+				e.printStackTrace();
+			}
 	}
 
 }
